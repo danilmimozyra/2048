92ebd5dc02b064b92320dde72c8a2bf2a3f1b045
Danylo Lazurko <163033184+danilmimozyra@users.noreply.github.com>
2026-04-27 21:41:44 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,12 +19,12 @@ export default defineConfig({
         start_url: "/2048/",
         icons: [
           {
-            src: "/icon-192.png",
+            src: "/2048/icon-192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icon-512.png",
+            src: "/2048/icon-512.png",
             sizes: "512x512",
             type: "image/png",
           },
