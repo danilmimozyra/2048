63eaac48d39a1537dd169d26c15de11e9d3074be
pocolor <luvecjr@gmail.com>
2026-04-28 09:07:01 +0200

    added score

---
+++ src/hooks/useBestScore.tsx
@@ -0,0 +1,31 @@
+import {useState} from "react";
+
+function useBestScore(score: number) {
+    const localStorageKey: string = "bestScore";
+
+    function setLocalBestScore(score: number) {
+        localStorage.setItem(localStorageKey, String(score));
+    }
+
+    function getLocalBestScore(): number {
+        const s = localStorage.getItem(localStorageKey);
+
+        if (s === null) {
+            setLocalBestScore(0);
+            return 0;
+        }
+
+        return Number(s);
+    }
+
+    const [bestScore, setBestScore] = useState<number>(() => getLocalBestScore());
+
+    if (score > bestScore) {
+        setLocalBestScore(score);
+        setBestScore(score);
+    }
+
+    return bestScore;
+}
+
+export default useBestScore;
